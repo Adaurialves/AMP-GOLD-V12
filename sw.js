ad70d4a5c218ad00222e2bff3268dfc1fe0c9ddc
Adaurialves <dau.alves19@gmail.com>
2026-03-09 20:21:56 -0300

    Implement service worker caching for assets

--- sw.js
+++ sw.js
@@ -1,1 +1,22 @@
-self.addEventListener('fetch', () => {});
+const CACHE_NAME = "gold-v4-cache";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/logo.jpg",
+  "/manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
